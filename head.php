e7177bf20184d2b539b8f5e373f4cb4c50b6b58d
thomweba <weba.web@gmail.com>
2012-10-19 14:07:34 +0900

    im test now
    i cant say nothing more

---
+++ head.php
@@ -0,0 +1,35 @@
+<!DOCTYPE html>
+<html lang="ja" xmlns:fb="http://ogp.me/ns/fb#" xmlns:og="http://ogp.me/ns#">
+<head>
+	<meta charset="UTF-8" />
+    <meta name="keywords" content="schoo,schoo WEB-campus,スクー,schoo.inc,ビジネスクリエイティブ,ビジネススクール">
+		<meta name="description" content="WEBに誕生した、学校の新しいカタチ。無料・WEB生中継で、様々な分野の授業を放送しています。現在は、新しいモノを生みだす力を養う「ビジネスクリエイティブ学部」が先行OPEN中！" />
+		<meta property="og:description" content="WEBに誕生した、学校の新しいカタチ。無料・WEB生中継で、様々な分野の授業を放送しています。現在は、新しいモノを生みだす力を養う「ビジネスクリエイティブ学部」が先行OPEN中！" />
+		<meta property="og:title" content="schoo（スクー） WEB-campus" />
+		<meta property="og:type" content="website" />
+		<meta property="og:image" content='http://cab.schoo.jp/img/logo_sq_small.png' />
+		<meta property="og:url" content="http://schoo.jp/" />
+		<meta property="fb:app_id" content="155430651192949" />
+        
+	<title>schoo（スクー） WEB-campus</title>
+    
+    <link rel="stylesheet" type="text/css" href="http://renew.schoo.jp/common/css/reset-min.css">
+	<link rel="stylesheet" type="text/css" href="http://renew.schoo.jp/common/css//layout.css" />
+	<link rel="stylesheet" type="text/css" href="http://renew.schoo.jp/common/css//class.css" />
+    
+	<!--[if lt IE 9]>
+	<script src="http://html5shiv.googlecode.com/svn/trunk/html5.js"></script>
+	<![endif]-->
+    
+</head>
+<body class="hogehoge">
+	
+<div id="fb-root"></div>
+
+<script>(function(d, s, id) {
+  var js, fjs = d.getElementsByTagName(s)[0];
+  if (d.getElementById(id)) return;
+  js = d.createElement(s); js.id = id;
+  js.src = "//connect.facebook.net/ja_JP/all.js#xfbml=1&appId=155430651192949";
+  fjs.parentNode.insertBefore(js, fjs);
+}(document, 'script', 'facebook-jssdk'));</script>
---
+++ index.php
@@ -0,0 +1,9 @@
+<?php
+$title = "トップページ";
+include("../module/head.php");
+include("../module/header.php");
+include("../module/nav.php");
+?>
+
+<?php include("../module/footer.php"); ?>
+<?php include("../module/foot.php"); ?>
